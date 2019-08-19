3db5c761b246721aacfb217404b92522931facdc
Abdelmonsif <39737270+Abdelmonsif@users.noreply.github.com>
2019-08-19 17:30:12 -0500

    Update run_mapping.sh

--- hpc/run_mapping.sh
+++ hpc/run_mapping.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=/home/syang29/.conda/envs/my_env/lib/python2.7/site-packages:$PYTHONPATH
-cd /work/syang29/Monsif_pbs/
+export PYTHONPATH=/home/$USER/.conda/envs/my_env/lib/python2.7/site-packages:$PYTHONPATH
+cd /work/$USER/Mapping_SNPs-master/hpc/
 python mapping_serial.py -inputCSV $1 -out output/$2.out 2>&1
